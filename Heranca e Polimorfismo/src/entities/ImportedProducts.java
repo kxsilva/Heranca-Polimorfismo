50f8a3ed469d0b3c23781084b5e320ee72d646f6
Kaynã Xavier <kayna.xavier@ufpe.br>
2022-06-21 16:49:26 -0300

    Exercicio Heranca e Polimorfismo

---
+++ Heranca e Polimorfismo/src/entities/ImportedProducts.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class ImportedProducts extends Product{
+	
+	private Double customsFee;
+	
+	public ImportedProducts() {
+		super();
+	}
+
+	public ImportedProducts(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	public double totalPrice() {
+		return super.getPrice() + customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return super.priceTag() + "(Customs fee: : $" + String.format("%.2f", customsFee);
+	}
+
+}
